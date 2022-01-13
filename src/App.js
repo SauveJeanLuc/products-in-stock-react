71b38e40e42974b277e23603f65a6b9bf5d56da2
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-13 15:10:53 +0200

    Embedded ProductsTable Component in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react'
-
+import ProductsTable from './components/ProductsTable'
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <ProductsTable />
+    </>
   )
 }
 
