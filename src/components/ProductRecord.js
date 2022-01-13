f5ec02cd389174ae7b9e1a05ecacadd5f81a90e4
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-13 15:07:02 +0200

    ProductRecord Component Defined

---
+++ src/components/ProductRecord.js
@@ -0,0 +1,12 @@
+const ProductRecord = () => {
+    
+    return (
+        <tr>
+            <td>Football</td>
+            <td>$49.99</td>
+        </tr>
+    )
+
+}
+
+export default ProductRecord;
