41fc344cba7a27f66cbcfd3f4ec00f429f9e3bdf
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-13 15:09:30 +0200

    ProductCategoryRow Component Initiated

---
+++ src/components/ProductCategoryRow.js
@@ -0,0 +1,10 @@
+const ProductCategoryRow = () => {
+    
+    return (
+        <tr>
+            <td style={{fontWeight: 'bold'}}>Sporting Goods</td>
+        </tr>
+    )
+}
+
+export default ProductCategoryRow;
