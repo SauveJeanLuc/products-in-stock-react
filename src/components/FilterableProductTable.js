ad6b4e82f760265c496e80041c00aac0ef90eb5e
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-14 11:21:51 +0200

    Defined FilterableProductTable

---
+++ src/components/FilterableProductTable.js
@@ -0,0 +1,12 @@
+import SearchBar from "./SearchBar";
+import ProductsTable from "./ProductsTable";
+const FilterableProductTable = () => {
+    return (
+        <>
+            <SearchBar />
+            <ProductsTable />
+        </>
+    )
+} 
+
+export default FilterableProductTable;
