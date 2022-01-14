da2035c0871fd5b184f04a88c3c4b3a56be8bd32
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-14 11:02:55 +0200

    Change ProductRecords color on stocked prop's value

--- src/components/ProductRecord.js
+++ src/components/ProductRecord.js
@@ -1,12 +1,20 @@
-const ProductRecord = () => {
+import PropTypes from 'prop-types'
+
+const ProductRecord = ({ name, price, stocked }) => {
     
     return (
-        <tr>
-            <td>Football</td>
-            <td>$49.99</td>
+        <tr style={stocked && {color: 'red'}}>
+            <td>{ name }</td>
+            <td>{ price }</td>
         </tr>
     )
 
+}
+
+ProductRecord.propTypes = {
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    stock: PropTypes.bool,
 }
 
 export default ProductRecord;
