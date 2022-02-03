7344dcfa4080197e3302aa2b2e35d7e8a0191f37
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-02-03 20:45:01 +0200

    Set SearchBar to Handling FilterTextChange and InStockOnly Flag change

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,23 @@
-const SearchBar = () => {
+import { useState } from "react";
+
+const SearchBar = ({filterText, inStockOnly}) => {
+    const [handleFilterTextChange, setHandleFilterTextChange] = useState[this.handleFilterTextChange.bind(this)];
+    const [handleInStockChange, setHandleInStockChange] = useState[this.handleInStockChange.bind(this)];
+
+    function handleFilterTextChange(e) {
+        this.props.onFilterTextChange(e.target.value);
+      }
+      
+    function handleInStockChange(e) {
+        this.props.onInStockChange(e.target.checked);
+    }
+
     return (
         <div>
-            <input type="text" placeholder="Search..." />
+            <input type="text" placeholder="Search..." value={filterText}/>
             <p>
-                <input type="checkbox" name="show_product" id="show_product" />
+                <input type="checkbox" checked={inStockOnly} name="show_product" id="show_product" />
+                {console.log("In Stock Only value: "+ inStockOnly)}
                 <span> Only show products in stock</span>
             </p>
         </div>
